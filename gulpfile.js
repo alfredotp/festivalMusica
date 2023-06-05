85730d9e7e9d2742eb80bd45438262b32c6b5c35
alfredo <dalfredot@gmail.com>
2023-06-04 22:34:09 -0500

    Aligerar imágenes con Gulp

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// function tarea(done) {
+//   console.log("mi primer tarea");
+
+//   done();
+// }
+
+// exports.tarea = tarea;
+
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+
+// Imagenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+
+function css(done) {
+  src("src/scss/**/*.scss") // Identificar el archivo de SASS
+    .pipe(plumber())
+    .pipe(sass()) // Compilarlo
+    .pipe(dest("build/css")); // Almacenarla en el disco duro
+
+  done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3,
+  };
+
+  src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest("build/img"));
+
+  done();
+}
+
+function versionWebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+
+  src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+
+  done();
+}
+
+function dev(done) {
+  watch("src/scss/**/*.scss", css);
+  done();
+}
+
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
